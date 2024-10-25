7a34889b357e59788aae8ebe21097bc66a23447a
PanDiablo <163389043+nikzavadskiy@users.noreply.github.com>
2024-10-25 15:42:46 +0400

    Update mbcb.py

--- mbcb.py
+++ mbcb.py
@@ -7,27 +7,33 @@ def get_smt_from_cmd() -> str:
     получает из командной строки: название файла и имя для подсчета
     """
     parser = argparse.ArgumentParser() 
-    parser.add_argument('filename', type=str, help='file that needs to open')
-    parser.add_argument('humanname', type=str, help='name for find how many people same have it')
+    parser.add_argument('file_name', type=str, help='file that needs to open')
+    parser.add_argument('human_name', type=str, help='name for find how many people same have it')
     return parser.parse_args()
 
 
-def read(filename: str) -> str:
+def read(file_name: str) -> str:
     """
      считывает файл
      :param filename: имя исх. файла
     :return: строку со всеми данными из считываемого файла
     """
     try:
-        with open(filename, "r", encoding="utf-8") as file:
+        with open(file_name, "r", encoding="utf-8") as file:
             text = file.read()
         return text
-    except FileNotFoundError as erorrreading:
-        raise FileNotFoundError(f"Ошибка: файл с именем '{filename}'  не найден : {erorrreading}")
+    except file_not_found as erorr_reading:
+        raise file_not_found(f"Ошибка: файл с именем '{file_name}'  не найден : {erorr_reading}")
 
 
-def define_count(text: str, humanname:str) -> None:
-    print(text.count(humanname))
+def split_to_names (text:str)->list:
+    pattern = r'Имя:\s*([а-яА-Я]+)'
+    names=re.findall(pattern,text)
+    return names
+    
+
+def define_count(names: list, human_name:str) -> None:
+    print(text.count(human_name))
     
 
 def main():
@@ -35,9 +41,10 @@ def main():
     """
     try:
         arguments = get_smt_from_cmd()
-        txt = read(arguments.filename)
-        define_count(txt, arguments.humanname)
-    except Exception as erorr:
+        txt = read(arguments.file_name)
+        names = split_to_names(txt)
+        define_count(names, arguments.human_name)
+    except exception as erorr:
         print(f"Произошла ошибка: {erorr}")
 
 if __name__ == "__main__":
